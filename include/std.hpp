475a5ea2d31e1fc60860f79d4ac873a248bbe474
T2SU <40755203+T2SU@users.noreply.github.com>
2022-06-15 22:35:31 +0900

    std.hpp 에서 헤더 섹션 구분

--- include/std.hpp
+++ include/std.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
+/*****    External Libraries   *****/
+
+/* plog */
 #include <plog/Log.h>
 #include <plog/Init.h>
 #include <plog/Formatters/TxtFormatter.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
+
+/* cxxopts */
 #include <cxxopts.hpp>
-#include <iostream>
+
+
+/*****  Microsoft C++ Headers  *****/
+
 #include <Windows.h>
+
+
+/*****  Standard C++ Libraries *****/
+
+#include <iostream>
 #include <locale>
+
+
+/*****      Romi Libraries     *****/
+
 #include <hello.hpp>
